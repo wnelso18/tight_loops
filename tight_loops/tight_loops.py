73ea9859615d4c5bd7df99e5a8df8e0befd45d17
Will Nelson <wnelso18@vols.utk.edu>
2023-03-28 10:48:57 -0400

    fix

--- tight_loops/tight_loops.py
+++ tight_loops/tight_loops.py
@@ -2,6 +2,6 @@
 
 import random
 
-def generate_random_string(length):
+def generate_random_string(length=15):
     """Generates a random string."""
     return ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(length)])
